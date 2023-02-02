f9db5a8c4fc87a5bdd5740b5a81303cc27ffd45d
thivviyan98 <thivviyan1998@gmail.com>
2023-02-02 21:28:01 +0530

    update the server url

--- server/server.js
+++ server/server.js
@@ -43,4 +43,4 @@ app.post('/', async (req, res) => {
     }
 })
 
-app.listen(5000, () => console.log('server is running on port http://localhost:5000'));
+app.listen(5000, () => console.log('server is running on port https://kollo.onrender.com'));
