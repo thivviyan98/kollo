9d2129ba98900dd7c5ebb731382a30dd5dd47049
thivviyan98 <thivviyan1998@gmail.com>
2023-02-02 20:21:19 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -93,7 +93,7 @@ function generateUniqeId() {
 
     // featch data from server -> bot's response
 
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://kollo.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
